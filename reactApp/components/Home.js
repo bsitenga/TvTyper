bb4fe789d9cd753e69551f3097c20bb63dc9f49e
bsitenga <sitenga@usc.edu>
2019-07-27 20:57:23 -0400

    Created homepage skeleton

---
+++ reactApp/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Textbox from './Textbox';
+
+const Home = () => {
+    return (<div className = "home-container">
+        <div className = "left-tab">
+            <h1>Pick your show</h1>
+        </div>
+        <div className = "mid-circle">
+            <input type="text" placeholder="Filter..."></input>
+        </div>
+        <div className = "right-tab">
+            <h1>TV Typeracer</h1>
+            <Textbox />
+        </div>
+    </div>);
+}
+
+export default Home;
---
+++ reactApp/components/Textbox.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Textbox = () => {
+    return (<div className="textbox-container">
+        Quotes to fill in
+    </div>)
+}
+
+export default Textbox;
